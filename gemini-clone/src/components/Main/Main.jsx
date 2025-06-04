16d4ccb2888c14281510a73d1fd49e8ba355fd01
Alan <alanbiju1080@gmail.com>
2025-06-04 13:50:11 +0530

    Responsive Gemini: AI chatbot completed

--- gemini-clone/src/components/Main/Main.jsx
+++ gemini-clone/src/components/Main/Main.jsx
@@ -1,43 +1,76 @@
+import { useContext } from 'react'
 import { assets } from '../../assets/assets'
 import './Main.css'
+import { Context } from '../../context/Context'
 
-function Main(){
-    return(
+function Main() {
+
+    const { onSent, prevPrompts, setPrevPrompts, setRecentPrompt, recentPrompt, ShowResults, loading, resultData, input, setInput } = useContext(Context)
+
+
+    return (
         <div className="main">
             <div className="nav">
                 <p>Gemini</p>
                 <img src={assets.user_icon} alt="" />
             </div>
             <div className="main-container">
-                <div className="greet">
-                    <p><span>Hello Dev.</span></p>
-                    <p>How can I help you today</p>
-                </div>
-                <div className="cards">
-                    <div className="card">
-                        <p>Suggest beautiful places to visit on an upcoming Road Trip</p>
-                        <img src={assets.compass_icon} alt="" />
-                    </div><div className="card">
-                        <p>Briefly summarise this concept: Urban Planning</p>
-                        <img src={assets.bulb_icon} alt="" />
+                {!ShowResults ?
+                    <>
+                        <div className="greet">
+                            <p><span>Hello Dev.</span></p>
+                            <p>How can I help you today</p>
+                        </div>
+                        <div className="cards">
+                            <div className="card">
+                                <p>Suggest beautiful places to visit on an upcoming Road Trip</p>
+                                <img src={assets.compass_icon} alt="" />
+                            </div><div className="card">
+                                <p>Briefly summarise this concept: Urban Planning</p>
+                                <img src={assets.bulb_icon} alt="" />
+                            </div>
+                            <div className="card">
+                                <p>Brainstorm team bonding activities for our work retreat</p>
+                                <img src={assets.message_icon
+                                } alt="" />
+                            </div>
+                            <div className="card">
+                                <p>Improve the read ability of the following code</p>
+                                <img src={assets.code_icon} alt="" />
+                            </div>
+                        </div>
+                    </>
+                :
+                    <div className="result">
+                        <div className="result-title">
+                            <img src={assets.user_icon} alt="" />
+                            <p>{recentPrompt}</p>
+                        </div>
+                        <div className="result-data">
+                                <img src={assets.gemini_icon} alt="" />
+                                {loading ?
+                                    <div className="loader">
+                                        <hr />
+                                        <hr />
+                                        <hr />
+                                    </div>
+                                :
+                                    <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                                }
+                        </div>
                     </div>
-                    <div className="card">
-                        <p>Brainstorm team bonding activities for our work retreat</p>
-                        <img src={assets.message_icon
-                        } alt="" />
-                    </div>
-                    <div className="card">
-                        <p>Improve the read ability of the following code</p>
-                        <img src={assets.code_icon} alt="" />
-                    </div>
-                </div>
+                }
                 <div className="main-bottom">
                     <div className="search-box">
-                        <input type="text" placeholder='Enter your prompt'/>
+                        <input onChange={(e) => setInput(e.target.value)} onKeyDown={(e) => {
+                                    if (e.key === "Enter") {
+                                        onSent();
+                                    }}} value={input} type="text" placeholder='Enter your prompt' />
                         <div>
                             <img src={assets.gallery_icon} alt="" />
                             <img src={assets.mic_icon} alt="" />
-                            <img src={assets.send_icon} alt="" />
+                            {input ? <img onClick={() => onSent()} src={assets.send_icon} alt="" /> : null}
+                            
                         </div>
                     </div>
                     <p className="bottom-info">
--- gemini-clone/src/context/Context.jsx
+++ gemini-clone/src/context/Context.jsx
@@ -1,19 +1,77 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import runChat from "../config/gemini";
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
 
+  const [input,setInput] = useState("")
+  const [recentPrompt,setRecentPrompt] = useState("")
+  const [prevPrompts,setPrevPrompts] = useState([])
+  const [ShowResults,setShowResults] = useState(false)
+  const [loading,setLoading] = useState(false)
+  const [resultData,setResultData] = useState("")
+
+  const delayPara = (index,nextWord)=> {
+    setTimeout(function (){
+      setResultData(prev=>prev+nextWord);
+    }, 75*index)
+  }
+
   const onSent = async (prompt) => {
-    const result = await runChat(prompt);
+
+    setResultData("")
+    setLoading(true)
+    setShowResults(true)
+    let response
+    if(prompt !== undefined){
+      response = await runChat(prompt)
+      setRecentPrompt(prompt)
+    }
+    else{
+      setPrevPrompts(prev=>[...prev,input])
+      setRecentPrompt(input)
+      response =await runChat(input)
+    }
+    setRecentPrompt(input)
+    setPrevPrompts(prev=>[...prev,input])
+    
+    const result = await runChat(input);
     console.log("Gemini response:", result);
+    let responseArray = result.split("**")
+    let newResponse = "";
+    for(let i = 0; i < responseArray.length; i++){
+        if(i===0 || i%2 !== 1){
+            newResponse += responseArray[i] 
+        }
+        else{
+            newResponse += "<b>" + responseArray[i] + "</b>"
+        }
+    }
+    let newResponse2 = newResponse.split("*").join("</br>")
+
+    let newResponseArray = newResponse2.split(" ")
+    for(let i =0; i < newResponseArray.length-1; i++){
+        const nextWord = newResponseArray[i]
+        delayPara(i,nextWord+" ")
+    }
+    setLoading(false)
+    setInput("")
   };
 
-  onSent("Who is the Father of the nation of India");
+  // onSent("Who is the Father of the nation of India");
 
   const contextValue = {
-    onSent
+    onSent,
+    prevPrompts,
+    setPrevPrompts,
+    setRecentPrompt,
+    recentPrompt,
+    ShowResults,
+    loading,
+    resultData,
+    input,
+    setInput
   };
 
   return (
